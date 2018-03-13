8affc9a5ae739694f5fda4be540af522d68113a0
Christopher Neal <wandadars@gmail.com>
2018-03-13 17:41:47 -0400

    pep8 of MechManager

--- interfaces/cython/cantera/mixmaster/MechManager.py
+++ interfaces/cython/cantera/mixmaster/MechManager.py
@@ -21,48 +21,50 @@ _autoload = [
     (' H/O/Ar', 'h2o2.cti')
     ]
 
+
 def testit():
     pass
 
+
 class MechManager(Frame):
 
-    def __init__(self,master,app):
-        Frame.__init__(self,master)
+    def __init__(self, master, app):
+        Frame.__init__(self, master)
         #self.config(relief=GROOVE, bd=4)
         self.app = app
         self.master = master
-        self.mechindx = IntVar()
-        self.mechindx.set(1)
+        self.mech_index = IntVar()
+        self.mech_index.set(1)
 
         #m = Label(self, text = 'Loaded Mechanisms')
         #m.grid(column=0,row=0)
 #         m.bind('<Double-1>',self.show)
-#         self.mechindx.set(0)
+#         self.mech_index.set(0)
         self.mechanisms = []
         self.mlist = [ [] ]
         i = 1
         #for m in self.mechanisms:
-        #    self.mlist.append((m[0], self.setMechanism, 'check', self.mechindx, i))
+        #    self.mlist.append((m[0], self.set_mechanism, 'check', self.mech_index, i))
         #    i += 1
         #self.mlist.append([])
 
-        self.mechmenu = make_menu('Mixtures', self, self.mlist)
-        self.mechmenu.grid(row=0,column=0,sticky=W)
+        self.mech_menu = make_menu('Mixtures', self, self.mlist)
+        self.mech_menu.grid(row=0, column=0, sticky=W)
 
         self.mfr = None
 
-    def addMechanism(self, name, mech):
+    def add_mechanism(self, name, mech):
         self.mechanisms.append((name, mech))
         il = len(self.mechanisms)
-        self.mlist[-1] = (name, self.setMechanism, 'check', self.mechindx, il)
-        add_menu_item(list(self.mechmenu.children.values())[0], self.mlist[-1])
+        self.mlist[-1] = (name, self.set_mechanism, 'check', self.mech_index, il)
+        add_menu_item(list(self.mech_menu.children.values())[0], self.mlist[-1])
         self.mlist.append([])
 
-        self.mechindx.set(il)
-        self.mechmenu.grid(row=0,column=0,sticky=W)
+        self.mech_index.set(il)
+        self.mech_menu.grid(row=0, column=0, sticky=W)
 
 
-    def delMechanism(self, mech):
+    def del_mechanism(self, mech):
         self.mechanisms.remove(mech)
         self.show()
 
@@ -76,15 +78,15 @@ class MechManager(Frame):
 ##         Label(self.mfr,text='jkl').grid(row=0,column=0)
 ##         i = 0
 ##         for name, mech in self.mechanisms:
-##             Radiobutton(self.mfr, text=name, variable=self.mechindx,
+##             Radiobutton(self.mfr, text=name, variable=self.mech_index,
 ##                         value = i,
-##                         command=self.setMechanism).grid(row=i,column=0)
+##                         command=self.set_mechanism).grid(row=i,column=0)
 ##             i += 1
 ##         print 'end'
 
 
-    def setMechanism(self, event=None):
-        i = self.mechindx.get()
+    def set_mechanism(self, event=None):
+        i = self.mech_index.get()
         self.app.mech = self.mechanisms[i-1][1]
         self.app.make_mix()
         self.app.makeWindows()

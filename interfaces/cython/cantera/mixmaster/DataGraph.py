7a0f0ee3cd925dedcde3f8bcabe64c8c08332108
Christopher Neal <wandadars@gmail.com>
2018-03-13 19:18:07 -0400

    pep8 second pass over DataGraph.py

--- interfaces/cython/cantera/mixmaster/DataGraph.py
+++ interfaces/cython/cantera/mixmaster/DataGraph.py
@@ -2,14 +2,16 @@
 # at http://www.cantera.org/license.txt for license and copyright information.
 
 import sys
+import math
+import numpy as np
+
 
 if sys.version_info[0] == 3:
     from tkinter import *
 else:
     from Tkinter import *
 
-import math
-from numpy import *
+
 
 def plotLimits(ypts, f=0.0, ndiv=5, logscale=0):
     """Return plot limits that"""
@@ -29,7 +31,7 @@ def plotLimits(ypts, f=0.0, ndiv=5, logscale=0):
 
     if logscale:
         ymin = math.floor(math.log10(ymin))
-        ymax = math.floor(math.log10(ymax))+1
+        ymax = math.floor(math.log10(ymax)) + 1
         fctr = 1.0
 
 ##         if dy < 0.2*ymin:
@@ -46,26 +48,23 @@ def plotLimits(ypts, f=0.0, ndiv=5, logscale=0):
             p10 = math.floor(math.log10(0.1 * dy))
             fctr = math.pow(10.0, p10)
         except:
-            return (ymin - 1.0, ymax + 1.0, 1.0)
+            return ymin - 1.0, ymax + 1.0, 1.0
         mm = [2.0, 2.5, 2.0]
         i = 0
         while dy / fctr > ndiv:
             fctr = mm[i % 3] * fctr
             i += 1
-        ymin = fctr*math.floor(ymin / fctr)
-        ymax = fctr*(math.floor(ymax / fctr + 0.999))
+        ymin = fctr * math.floor(ymin / fctr)
+        ymax = fctr * (math.floor(ymax / fctr + 0.999))
 
-    return (ymin, ymax, fctr)
+    return ymin, ymax, fctr
 
 
 class DataGraph(Frame):
-    def __init__(self,master,
-                 data, ix=0, iy=0,
-                 title='',
-                 label = ('x-axis', 'y-axis'),
-                 logscale = (0, 0),
-                 pixelX=500,
-                 pixelY=500):
+    def __init__(self, master, data, ix=0, iy=0, title='',
+                 label=('x-axis', 'y-axis'), logscale=(0, 0),
+                 pixelX=500, pixelY=500):
+
         self.logscale = logscale
         self.data = data
         self.ix = ix
@@ -87,10 +86,10 @@ class DataGraph(Frame):
         id = self.canvas.create_rectangle(self.origin[0], self.origin[1],
                                           pixelX-20, pixelY-50)
         self.canvas.grid(row=1, column=1, rowspan=2, sticky=N+S+E+W)
-        self.last_points=[]
+        self.last_points = []
         self.ticks(self.minX, self.maxX, self.dx,
                    self.minY, self.maxY, self.dy, 10)
-        self.screendata()
+        self.screen_data()
         self.draw()
         self.canvas.create_text(self.origin[0] + self.graph_w/2,
                                 self.origin[1] + self.graph_h + 30,
@@ -99,54 +98,60 @@ class DataGraph(Frame):
                                 self.origin[1] + self.graph_h/2,
                                 text=label[1], anchor=E)
 
+        self.x_data = []
+        self.y_data = []
 
-    def writeValue(self, y):
-        yval = '%15.4f' % (y)
-        self.title.config(text=yval)
+    def write_value(self, y):
+        y_val = '%15.4f' % y
+        self.title.config(text=y_val)
 
     def delete(self, ids):
-        for id in ids:
-            self.canvas.delete(id)
+        for id_num in ids:
+            self.canvas.delete(id_num)
 
-    def screendata(self):
-        self.xdata = array(self.data[self.ix, :])
-        self.ydata = array(self.data[self.iy, :])
-        npts = len(self.ydata)
-        if self.logscale[0] > 0:
-            self.xdata = log10(self.xdata)
-        if self.logscale[1] > 0:
-            self.ydata = log10(self.ydata)
-        f = float(self.graph_w)/(self.maxX-self.minX)
-        self.xdata = (self.xdata - self.minX)*f + self.origin[0]
-        f = float(self.graph_h)/(self.maxY-self.minY)
-        self.ydata = (self.maxY - self.ydata)*f + self.origin[1]
+    def screen_data(self):
+        self.x_data = np.array(self.data[self.ix, :])
+        self.y_data = np.array(self.data[self.iy, :])
 
-    def toscreen(self, x, y):
         if self.logscale[0] > 0:
-            x = log10(x)
+            self.x_data = np.log10(self.x_data)
         if self.logscale[1] > 0:
-            y = log10(y)
-        f = float(self.graph_w)/(self.maxX-self.minX)
-        xx = (x - self.minX)*f + self.origin[0]
-        f = float(self.graph_h)/(self.maxY-self.minY)
-        yy = (self.maxY - y)*f + self.origin[1]
+            self.y_data = np.log10(self.y_data)
+
+        f = float(self.graph_w) / (self.maxX - self.minX)
+        self.x_data = (self.x_data - self.minX) * f + self.origin[0]
+
+        f = float(self.graph_h) / (self.maxY - self.minY)
+        self.y_data = (self.maxY - self.y_data) * f + self.origin[1]
+
+    def to_screen(self, x, y):
+        if self.logscale[0] > 0:
+            x = np.log10(x)
+        if self.logscale[1] > 0:
+            y = np.log10(y)
+
+        f = float(self.graph_w) / (self.maxX - self.minX)
+        xx = (x - self.minX) * f + self.origin[0]
+
+        f = float(self.graph_h) / (self.maxY - self.minY)
+        yy = (self.maxY - y) * f + self.origin[1]
         return xx, yy
 
-    def move(self, id, newpos, oldpos):
-        dxpt = (newpos[0] - oldpos[0])/(self.maxX-self.minX)*self.graph_w
-        dypt = -(newpos[1] - oldpos[1])/(self.maxY-self.minY)*self.graph_h
+    def move(self, id, new_pos, old_pos):
+        dxpt = (new_pos[0] - old_pos[0]) / (self.maxX - self.minX) * self.graph_w
+        dypt = -(new_pos[1] - old_pos[1]) / (self.maxY - self.minY) * self.graph_h
+
         self.canvas.move(id, dxpt, dypt)
-        self.writeValue(newpos[1])
+        self.write_value(new_pos[1])
 
     def plot(self, n, color='black'):
-        xpt, ypt = self.toscreen(self.data[self.ix, n],
-                                 self.data[self.iy, n])
+        xpt, ypt = self.to_screen(self.data[self.ix, n], self.data[self.iy, n])
         #xpt = (x-self.minX)/(self.maxX-self.minX)*float(self.graph_w) + self.origin[0]
         #ypt = (self.maxY-y)/(self.maxY-self.minY)*float(self.graph_h) + self.origin[1]
         id_ycross = self.canvas.create_line(xpt, self.graph_h+self.origin[1], xpt, self.origin[1], fill='gray')
         id_xcross = self.canvas.create_line(self.origin[0], ypt, self.graph_w+self.origin[0], ypt, fill='gray')
         id = self.canvas.create_oval(xpt-2, ypt-2, xpt+2, ypt+2, fill=color)
-        #self.writeValue(y)
+        #self.write_value(y)
         s = '(%g, %g)' % (self.data[self.ix, n], self.data[self.iy, n])
         if n > 0 and self.data[self.iy, n] > self.data[self.iy, n-1]:
             idt = self.canvas.create_text(xpt+5, ypt+5, text=s, anchor=NW)
@@ -156,84 +161,83 @@ class DataGraph(Frame):
         return [id, id_xcross, id_ycross, idt]
 
     def draw(self, color='red'):
-        npts = len(self.xdata)
+        npts = len(self.x_data)
         for n in range(1, npts):
-            self.canvas.create_line(self.xdata[n-1], self.ydata[n-1],
-                            self.xdata[n], self.ydata[n], fill=color)
+            self.canvas.create_line(self.x_data[n - 1], self.y_data[n - 1],
+                                    self.x_data[n], self.y_data[n], fill=color)
 
-    def addLabel(self, y, orient=0):
+    def add_label(self, y, orient=0):
         if orient == 0:
-            xpt, ypt = self.toscreen(y, 1.0)
+            xpt, ypt = self.to_screen(y, 1.0)
             ypt = self.origin[1] + self.graph_h + 5
             self.canvas.create_text(xpt, ypt, text=y, anchor=N)
         else:
-            xpt, ypt = self.toscreen(self.minX, y)
+            xpt, ypt = self.to_screen(self.minX, y)
             xpt = self.origin[0] - 5
             self.canvas.create_text(xpt, ypt, text=y, anchor=E)
-    def addLegend(self, text, color=None):
-        m=Message(self, text=text, width=self.graph_w-10)
+
+    def add_legend(self, text, color=None):
+        m = Message(self, text=text, width=self.graph_w - 10)
         m.pack(side=BOTTOM)
         if color:
             m.config(fg=color)
 
-    def pauseWhenFinished(self):
+    def pause_when_finished(self):
         self.wait_window()
 
-    def minorTicks(self, x0, x1, y, n, size, orient=0):
-        xtick = x0
+    def minor_ticks(self, x0, x1, y, n, size, orient=0):
+        x_tick = x0
         dx = (x1 - x0) / float(n)
         if orient == 0:
-            while xtick <= x1:
-                xx, yy = self.toscreen(xtick, y)
-                self.canvas.create_line(xx, yy,
-                                        xx, yy-size)
-                xtick += dx
+            while x_tick <= x1:
+                xx, yy = self.to_screen(x_tick, y)
+                self.canvas.create_line(xx, yy, xx, yy-size)
+                x_tick += dx
         else:
-            while xtick <= x1:
-                xx, yy = self.toscreen(y, xtick)
-                self.canvas.create_line(xx, yy,
-                                        xx + size, yy)
-                xtick += dx
+            while x_tick <= x1:
+                xx, yy = self.to_screen(y, x_tick)
+                self.canvas.create_line(xx, yy, xx + size, yy)
+                x_tick += dx
 
-
-    def ticks(self, xmin, xmax, dx, ymin, ymax, dy, size):
+    def ticks(self, x_min, x_max, dx, y_min, y_max, dy, size):
 
         if self.logscale[0]:
-            xmin = math.pow(10.0, xmin)
-            xmax = math.pow(10.0, xmax)
+            x_min = math.pow(10.0, x_min)
+            x_max = math.pow(10.0, x_max)
         if self.logscale[1]:
-            ymin = math.pow(10.0, ymin)
-            ymax = math.pow(10.0, ymax)
+            y_min = math.pow(10.0, y_min)
+            y_max = math.pow(10.0, y_max)
 
         n = 5
-        ytick = ymin
-        while ytick <= ymax:
-            xx, yy = self.toscreen(xmin, ytick)
+        ytick = y_min
+        while ytick <= y_max:
+            xx, yy = self.to_screen(x_min, ytick)
             self.canvas.create_line(xx, yy, xx + size, yy)
-            self.addLabel(ytick, 1)
-            xx, yy = self.toscreen(xmax, ytick)
+            self.add_label(ytick, 1)
+
+            xx, yy = self.to_screen(x_max, ytick)
             self.canvas.create_line(xx, yy, xx - size, yy)
             ytick0 = ytick
             if self.logscale[1]:
                 ytick *= 10.0
                 n = 10
             else: ytick += dy
-            if ytick <= ymax:
-                self.minorTicks(ytick0, ytick, xmin, n, 5, 1)
-                self.minorTicks(ytick0, ytick, xmax, n, -5, 1)
+            if ytick <= y_max:
+                self.minor_ticks(ytick0, ytick, x_min, n, 5, 1)
+                self.minor_ticks(ytick0, ytick, x_max, n, -5, 1)
 
         n = 5
-        xtick = xmin
-        while xtick <= xmax:
-            xx, yy = self.toscreen(xtick, ymin)
+        xtick = x_min
+        while xtick <= x_max:
+            xx, yy = self.to_screen(xtick, y_min)
             self.canvas.create_line(xx, yy, xx, yy - size)
-            self.addLabel(xtick, 0)
-            xx, yy = self.toscreen(xtick, ymax)
+            self.add_label(xtick, 0)
+            xx, yy = self.to_screen(xtick, y_max)
             self.canvas.create_line(xx, yy, xx, yy + size)
             if self.logscale[0]:
                 xtick *= 10.0
                 n = 10
             else: xtick += dx
-            if xtick <= xmax:
-                self.minorTicks(xtick - dx, xtick, ymin, n, 5, 0)
-                self.minorTicks(xtick - dx, xtick, ymax, n, -5, 0)
+            if xtick <= x_max:
+                self.minor_ticks(xtick - dx, xtick, y_min, n, 5, 0)
+                self.minor_ticks(xtick - dx, xtick, y_max, n, -5, 0)

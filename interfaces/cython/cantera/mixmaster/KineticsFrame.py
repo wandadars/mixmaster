992ee98074811b3e16d43ae9952d5eee2bb1b71e
Christopher Neal <wandadars@gmail.com>
2018-03-13 18:17:41 -0400

    pep8 of KineticsFrame

--- interfaces/cython/cantera/mixmaster/KineticsFrame.py
+++ interfaces/cython/cantera/mixmaster/KineticsFrame.py
@@ -3,30 +3,35 @@
 
 from __future__ import print_function
 
-import os, math, sys
+import webbrowser
+import os
+import math
+import sys
+
+from cantera import *
+
+from SpeciesInfo import SpeciesInfo
 
 if sys.version_info[0] == 3:
     from tkinter import *
 else:
     from Tkinter import *
 
-from cantera import *
-
-from .SpeciesInfo import SpeciesInfo
-import webbrowser
 
 _CUTOFF = 1.e-15
 _ATOL = 1.e-15
 _RTOL = 1.e-7
 
-def showsvg():
+
+def show_svg():
     f = open('_rp_svg.html','w')
     f.write('<embed src="rxnpath.svg" name="rxnpath" height=500\n')
     f.write('type="image/svg-xml" pluginspage="http://www.adobe.com/svg/viewer/install/">\n')
     f.close()
     webbrowser.open('file:///'+os.getcwd()+'/_rp_svg.html')
 
-def showpng():
+
+def show_png():
     f = open('_rp_png.html','w')
     f.write('<img src="rxnpath.png" height=500/>\n')
     f.close()
@@ -34,8 +39,8 @@ def showpng():
 
 
 class KineticsFrame(Frame):
-    def __init__(self,master):
-        Frame.__init__(self,master)
+    def __init__(self, master):
+        Frame.__init__(self, master)
         self.config(relief=FLAT, bd=4)
         self.top = self.master.top
         self.controls=Frame(self)
@@ -44,19 +49,19 @@ class KineticsFrame(Frame):
         self.comp = IntVar()
         self.comp.set(2)
         self.controls.grid(column=1,row=0,sticky=W+E+N)
-        self.makeControls()
+        self.make_controls()
         mf = self.master
 
-    def makeControls(self):
-        Radiobutton(self.controls,text='Creation Rates',
-                    variable=self.comp,value=0,
-                    command=self.show).grid(column=0,row=0,sticky=W)
-        Radiobutton(self.controls,text='Destruction Rates',
-                    variable=self.comp,value=1,
-                    command=self.show).grid(column=0,row=1,sticky=W)
-        Radiobutton(self.controls,text='Net Production Rates',
-                    variable=self.comp,value=2,
-                    command=self.show).grid(column=0,row=2,sticky=W)
+    def make_controls(self):
+        Radiobutton(self.controls, text='Creation Rates',
+                    variable=self.comp, value=0,
+                    command=self.show).grid(column=0, row=0, sticky=W)
+        Radiobutton(self.controls, text='Destruction Rates',
+                    variable=self.comp, value=1,
+                    command=self.show).grid(column=0, row=1, sticky=W)
+        Radiobutton(self.controls, text='Net Production Rates',
+                    variable=self.comp, value=2,
+                    command=self.show).grid(column=0, row=2, sticky=W)
 
     def show(self):
         mf = self.master
@@ -66,12 +71,12 @@ class KineticsFrame(Frame):
         g = mix.g
         if c == 0:
             mf.var.set("Creation Rates")
-            #mf.data = spdict(mix.g, mix.moles())
+            #mf.data = spdict(mix.gas, mix.moles())
             mf.comp = g.creation_rates
 
         elif c == 1:
             mf.var.set("Destruction Rates")
-            #mf.data = spdict(mix.g,mix.mass())
+            #mf.data = spdict(mix.gas,mix.mass())
             mf.comp = g.destruction_rates
 
         elif c == 2:
@@ -89,9 +94,10 @@ class KineticsFrame(Frame):
             except:
                 pass
 
+
 class SpeciesKineticsFrame(Frame):
-    def __init__(self,master,top):
-        Frame.__init__(self,master)
+    def __init__(self, master, top):
+        Frame.__init__(self, master)
         self.config(relief=GROOVE, bd=4)
         self.top = top
         self.top.kinetics = self
@@ -99,18 +105,19 @@ class SpeciesKineticsFrame(Frame):
         self.entries=Frame(self)
         self.var = StringVar()
         self.var.set("Net Production Rates")
-        self.names = self.top.mix.speciesNames()
+        self.names = self.top.mix.species_names()
         self.nsp = len(self.names)
-        self.comp = [0.0]*self.nsp
-        self.makeControls()
-        self.makeEntries()
-        self.entries.bind('<Double-l>',self.minimize)
+        self.comp = [0.0] * self.nsp
+        self.make_controls()
+        self.make_entries()
+        self.entries.bind('<Double-l>', self.minimize)
         self.ctype = 0
+        self.variable = {}
 
-    def makeControls(self):
+    def make_controls(self):
         self.c = KineticsFrame(self)
         #self.rr = ReactionKineticsFrame(self, self.top)
-        self.c.grid(column=1,row=0,sticky=E+W+N+S)
+        self.c.grid(column=1, row=0, sticky=E+W+N+S)
         #self.rr.grid(column=0,row=1,sticky=E+W+N+S)
 
     def show(self):
@@ -119,48 +126,47 @@ class SpeciesKineticsFrame(Frame):
     def redo(self):
         self.update()
         self.entries.destroy()
-        self.entries=Frame(self)
-        self.makeEntries()
+        self.entries = Frame(self)
+        self.make_entries()
 
-    def minimize(self,Event=None):
+    def minimize(self, Event=None):
         self.c.hide.set(1)
         self.redo()
         self.c.grid_forget()
-        self.entries.bind("<Double-1>",self.maximize)
+        self.entries.bind("<Double-1>", self.maximize)
 
-    def maximize(self,Event=None):
+    def maximize(self, Event=None):
         self.c.hide.set(0)
         self.redo()
-        self.c.grid(column=1,row=0,sticky=E+W+N+S)
-        self.entries.bind("<Double-1>",self.minimize)
+        self.c.grid(column=1, row=0, sticky=E+W+N+S)
+        self.entries.bind("<Double-1>", self.minimize)
 
     def up(self, x):
         self.update()
 
-    def makeEntries(self):
-        self.entries.grid(row=0,column=0,sticky=W+N+S+E)
-        self.entries.config(relief=FLAT,bd=4)
-        DATAKEYS = self.top.species
-        self.variable = {}
+    def make_entries(self):
+        self.entries.grid(row=0, column=0, sticky=W+N+S+E)
+        self.entries.config(relief=FLAT, bd=4)
+        data_keys = self.top.species
 
         n=0
         ncol = 3
         col = 0
         row = 60
 
-        for sp in DATAKEYS:
+        for sp in data_keys:
             s = sp
             k = s.index
             if row > 15:
                 row = 0
                 col += 2
-                l = Label(self.entries,text='Species')
-                l.grid(column=col,row=row,sticky=E+W)
+                l = Label(self.entries, text='Species')
+                l.grid(column=col, row=row, sticky=E+W)
                 e1 = Entry(self.entries)
-                e1.grid(column=col+1,row=row,sticky=E+W)
+                e1.grid(column=col+1, row=row, sticky=E+W)
                 e1['textvariable'] = self.var
                 e1.config(state=DISABLED)
-                e1.config(bg='lightyellow',relief=RIDGE)
+                e1.config(bg='lightyellow', relief=RIDGE)
                 row += 1
 
             spname = s.name
@@ -168,20 +174,20 @@ class SpeciesKineticsFrame(Frame):
             if not self.c.hide.get() or val: showit = 1
             else:  showit = 0
 
-            l=SpeciesInfo(self.entries,species=s,
-                          text=spname,relief=FLAT,justify=RIGHT,
+            l = SpeciesInfo(self.entries,species=s,
+                          text=spname, relief=FLAT, justify=RIGHT,
                           fg='darkblue')
             entry1 = Entry(self.entries)
             self.variable[spname] = DoubleVar()
             self.variable[spname].set(self.comp[k])
             entry1['textvariable']=self.variable[spname]
-            entry1.bind('<Any-Leave>',self.up)
+            entry1.bind('<Any-Leave>', self.up)
             if showit:
-                l.grid(column= col ,row=row,sticky=E)
-                entry1.grid(column=col+1,row=row)
+                l.grid(column= col, row=row, sticky=E)
+                entry1.grid(column=col+1, row=row)
                 n += 1
                 row += 1
-                entry1.config(state=DISABLED,bg='lightgray')
+                entry1.config(state=DISABLED, bg='lightgray')
 
 
 class ReactionKineticsFrame(Frame):
@@ -194,7 +200,7 @@ class ReactionKineticsFrame(Frame):
         self.top = top
         self.g = self.top.mix.g
         nr = self.g.n_reactions
-        self.eqs=Text(self,width=40,height=30)
+        self.eqs=Text(self, width=40, height=30)
         self.data = []
         self.start = DoubleVar()
         if nr > 30:
@@ -203,35 +209,35 @@ class ReactionKineticsFrame(Frame):
             self.end = self.start.get()+nr
 
         for i in range(4):
-            self.data.append(Text(self,width=15,height=30))
+            self.data.append(Text(self, width=15, height=30))
 
         for n in range(nr):
             s = self.g.reaction_equation(n)
             self.eqs.insert(END,s+'\n')
-        self.eqs.grid(column=0,row=1,sticky=W+E+N)
+        self.eqs.grid(column=0, row=1, sticky=W+E+N)
         for i in range(4):
-            self.data[i].grid(column=i+1,row=1,sticky=W+E+N)
-        Label(self, text='Reaction').grid(column=0,row=0,sticky=W+E+N)
-        Label(self, text='Fwd ROP').grid(column=1,row=0,sticky=W+E+N)
-        Label(self, text='Rev ROP').grid(column=2,row=0,sticky=W+E+N)
-        Label(self, text='Net ROP').grid(column=3,row=0,sticky=W+E+N)
-        Label(self, text='Kp').grid(column=4,row=0,sticky=W+E+N)
+            self.data[i].grid(column=i+1, row=1, sticky=W+E+N)
+        Label(self, text='Reaction').grid(column=0, row=0, sticky=W+E+N)
+        Label(self, text='Fwd ROP').grid(column=1, row=0, sticky=W+E+N)
+        Label(self, text='Rev ROP').grid(column=2, row=0, sticky=W+E+N)
+        Label(self, text='Net ROP').grid(column=3, row=0, sticky=W+E+N)
+        Label(self, text='Kp').grid(column=4, row=0, sticky=W+E+N)
 
         self.scfr = Frame(self)
-        self.scfr.config(relief=GROOVE,bd=4)
+        self.scfr.config(relief=GROOVE, bd=4)
 
 ##              self.sc = Scrollbar(self.scfr,command=self.show,
 ##                              variable = self.start,
 ##                              orient='horizontal',length=400)
         self.sc = Scale(self.scfr,command=self.show,
                         variable=self.start,
-                        orient='vertical',length=400)
+                        orient='vertical', length=400)
 #               self.sc.config(cnf={'from':0,'to':nr},variable = self.start)
         #self.sc.bind('<Any-Enter>',self.couple)
         #self.scfr.bind('<Any-Leave>',self.decouple)
-        self.sc.pack(side=RIGHT,fill=Y)
-        self.scfr.grid(row=0,column=6,rowspan=10,sticky=N+E+W)
-        self.grid(column=0,row=0)
+        self.sc.pack(side=RIGHT, fill=Y)
+        self.scfr.grid(row=0, column=6, rowspan=10, sticky=N+E+W)
+        self.grid(column=0, row=0)
 
         self.hide()
 
@@ -248,9 +254,9 @@ class ReactionKineticsFrame(Frame):
 #               self.vis.set(0)
         self.master.withdraw()
 
-    def show(self,e=None,b=None,c=None):
+    def show(self, e=None, b=None, c=None):
         v = self.vis.get()
-        print(e,b,c)
+        print(e, b, c)
         #if v == 0:
         #       self.hide()
         #       return
@@ -260,35 +266,36 @@ class ReactionKineticsFrame(Frame):
         frop = self.g.forward_rates_of_progress
         rrop = self.g.reverse_rates_of_progress
         kp = self.g.equilibrium_constants
-        self.data[0].delete(1.0,END)
-        self.data[1].delete(1.0,END)
-        self.data[2].delete(1.0,END)
-        self.data[3].delete(1.0,END)
-        self.eqs.delete(1.0,END)
+        self.data[0].delete(1.0, END)
+        self.data[1].delete(1.0, END)
+        self.data[2].delete(1.0, END)
+        self.data[3].delete(1.0, END)
+        self.eqs.delete(1.0, END)
 
         n0 = int(self.start.get())
         nn = nr - n0
         if nn > 30: nn = 30
         for n in range(n0, nn+n0):
             s = '%12.5e \n' % (frop[n],)
-            self.data[0].insert(END,s)
+            self.data[0].insert(END, s)
             s = '%12.5e \n' % (rrop[n],)
-            self.data[1].insert(END,s)
+            self.data[1].insert(END, s)
             s = '%12.5e \n' % (frop[n] - rrop[n],)
-            self.data[2].insert(END,s)
+            self.data[2].insert(END, s)
             s = '%12.5e \n' % (kp[n],)
-            self.data[3].insert(END,s)
+            self.data[3].insert(END, s)
             self.eqs.insert(END, self.g.reaction_equation(n)+'\n')
+
 
 class ReactionPathFrame(Frame):
 
-    def __init__(self,top):
+    def __init__(self, top):
         self.master = Toplevel()
         self.master.protocol("WM_DELETE_WINDOW",self.hide)
         #self.vis = vis
         Frame.__init__(self,self.master)
         self.config(relief=GROOVE, bd=4)
-        self.grid(column=0,row=0)
+        self.grid(column=0, row=0)
         self.top = top
         self.g = self.top.mix.g
         self.el = IntVar()
@@ -296,72 +303,73 @@ class ReactionPathFrame(Frame):
         self.thresh = DoubleVar()
 
         scframe = Frame(self)
-        self.sc = Scale(scframe,variable = self.thresh,
-                        orient='horizontal',digits=3,length=300,resolution=0.01)
+        self.sc = Scale(scframe, variable = self.thresh,
+                        orient='horizontal', digits=3, length=300, resolution=0.01)
         self.sc.config(cnf={'from':-6,'to':0})
-        Label(scframe,text='log10 Threshold').grid(column=0,row=0)
-        self.sc.grid(row=0,column=1,columnspan=10)
+        Label(scframe, text='log10 Threshold').grid(column=0, row=0)
+        self.sc.grid(row=0, column=1, columnspan=10)
         self.sc.bind('<ButtonRelease-1>',self.show)
-        scframe.grid(row=3,column=0,columnspan=10)
+        scframe.grid(row=3, column=0, columnspan=10)
 
         enames = self.g.element_names
         self.nel = len(enames)
 
         i = 1
         eframe = Frame(self)
-        Label(eframe,text='Element').grid(column=0,row=0,sticky=W)
+        Label(eframe, text='Element').grid(column=0, row=0, sticky=W)
         for e in enames:
-            Radiobutton(eframe,text=e,
-                        variable=self.el,value=i-1,
-                        command=self.show).grid(column=i,row=0,sticky=W)
+            Radiobutton(eframe, text=e,
+                        variable=self.el, value=i-1,
+                        command=self.show).grid(column=i, row=0, sticky=W)
             i += 1
-        eframe.grid(row=0,column=0)
+        eframe.grid(row=0, column=0)
 
         self.detailed = IntVar()
-        Checkbutton(self, text = 'Show details', variable=self.detailed,
-                 command=self.show).grid(column=1,row=0)
+        Checkbutton(self, text='Show details', variable=self.detailed,
+                    command=self.show).grid(column=1, row=0)
+
         self.net = IntVar()
-        Checkbutton(self, text = 'Show net flux',
+        Checkbutton(self, text='Show net flux',
                     variable=self.net,
-                    command=self.show).grid(column=2,row=0)
+                    command=self.show).grid(column=2, row=0)
+
         self.local = StringVar()
-        Label(self,text='Species').grid(column=1,row=1,sticky=E)
-        sp = Entry(self, textvariable=self.local,
-              width=15)
-        sp.grid(column=2,row=1)
-        sp.bind('<Any-Leave>',self.show)
+        Label(self,text='Species').grid(column=1, row=1, sticky=E)
+        sp = Entry(self, textvariable=self.local, width=15)
+        sp.grid(column=2, row=1)
+        sp.bind('<Any-Leave>', self.show)
 
 
 
         self.fmt = StringVar()
         self.fmt.set('svg')
-        i = 1
         fmtframe = Frame(self)
         fmtframe.config(relief=GROOVE, bd=4)
         self.browser = IntVar()
         self.browser.set(0)
-        Checkbutton(fmtframe, text = 'Display in Web Browser',
+        Checkbutton(fmtframe, text='Display in Web Browser',
                     variable=self.browser,
-                    command=self.show).grid(column=0,columnspan=6,row=0)
-        Label(fmtframe,text='Format').grid(column=0,row=1,sticky=W)
-        for e in ['svg', 'png', 'gif', 'jpg']:
-            Radiobutton(fmtframe,text=e,
-                        variable=self.fmt,value=e,
-                        command=self.show).grid(column=i,row=1,sticky=W)
-            i += 1
-        fmtframe.grid(row=5,column=0,columnspan=10,sticky=E+W)
+                    command=self.show).grid(column=0, columnspan=6, row=0)
+        Label(fmtframe, text='Format').grid(column=0, row=1, sticky=W)
 
-        self.cv = Canvas(self,relief=SUNKEN,bd=1)
-        self.cv.grid(column=0,row=4,sticky=W+E+N,columnspan=10)
+        for i, e in enumerate(['svg', 'png', 'gif', 'jpg']):
+            Radiobutton(fmtframe,text=e,
+                        variable=self.fmt, value=e,
+                        command=self.show).grid(column=i + 1, row=1, sticky=W)
+
+        fmtframe.grid(row=5, column=0, columnspan=10, sticky=E+W)
+
+        self.cv = Canvas(self, relief=SUNKEN, bd=1)
+        self.cv.grid(column=0, row=4, sticky=W+E+N, columnspan=10)
 
         pframe = Frame(self)
         pframe.config(relief=GROOVE, bd=4)
         self.dot = StringVar()
         self.dot.set('dot -Tgif rxnpath.dot > rxnpath.gif')
-        Label(pframe,text='DOT command:').grid(column=0,row=0,sticky=W)
-        Entry(pframe,width=60,textvariable=self.dot).grid(column=0,
-                                                 row=1,sticky=W)
-        pframe.grid(row=6,column=0,columnspan=10,sticky=E+W)
+        Label(pframe, text='DOT command:').grid(column=0, row=0, sticky=W)
+        Entry(pframe, width=60, textvariable=self.dot).grid(column=0,
+                                                 row=1, sticky=W)
+        pframe.grid(row=6, column=0, columnspan=10, sticky=E+W)
 
         self.thresh.set(-2.0)
         self.hide()
@@ -370,14 +378,16 @@ class ReactionPathFrame(Frame):
         #self.vis.set(0)
         self.master.withdraw()
 
-    def show(self,e=None):
+    def show(self, e=None):
 
         self.master.deiconify()
         el = self.g.element_name(self.el.get())
         det = False
-        if self.detailed.get() == 1: det = True
+        if self.detailed.get() == 1:
+            det = True
         flow = 'OneWayFlow'
-        if self.net.get() == 1: flow = 'NetFlow'
+        if self.net.get() == 1:
+            flow = 'NetFlow'
 
         self.d = ReactionPathDiagram(self.g, el)
         self.d.arrow_width = -2
@@ -396,8 +406,8 @@ class ReactionPathFrame(Frame):
         if self.browser.get() == 1:
             fmt = self.fmt.get()
             os.system('dot -T'+fmt+' rxnpath.dot > rxnpath.'+fmt)
-            if fmt == 'svg': showsvg()
-            elif fmt == 'png': showpng()
+            if fmt == 'svg': show_svg()
+            elif fmt == 'png': show_png()
             else:
                 path = 'file:///'+os.getcwd()+'/rxnpath.'+fmt
                 webbrowser.open(path)
@@ -418,7 +428,7 @@ class ReactionPathFrame(Frame):
                 self.cv.configure(width=self.rp.width(),
                                   height=self.rp.height())
 
-                self.image = self.cv.create_image(0,0,anchor=NW,
+                self.image = self.cv.create_image(0, 0, anchor=NW,
                                                   image=self.rp)
             except:
                 pass
